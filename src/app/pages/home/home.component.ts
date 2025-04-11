55f19c4a965e0c100027241bb7df8190196a4fed
marcelo.alves <marcelo.alves@pt.clara.net>
2025-04-11 16:12:04 +0100

    refactor: update home component to display formatted user information

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,25 +6,22 @@ import { AuthService } from '../../services/auth.service';
   template: `
     <div class="py-8">
       <h1 class="text-2xl font-bold mb-6">Bem-vindo, {{ userName }}</h1>
-      <pre>
-          {{ allInfo | json }}
-        </pre
-      >
+      <pre><code>{{ formattedInfo }}</code></pre>
     </div>
   `,
 })
 export class HomeComponent implements OnInit {
   userName = '';
-  allInfo: any = {};
+  formattedInfo = '';
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     const user = this.authService.getUser();
-    this.allInfo = user;
     user.then((userData) => {
       if (userData) {
         this.userName = userData.profile.name ?? 'Usuário';
+        this.formattedInfo = JSON.stringify(userData, null, 2);
       } else {
         this.userName = 'Usuário Desconhecido';
       }
