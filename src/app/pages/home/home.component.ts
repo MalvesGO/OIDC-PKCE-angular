9c19cc0b307bfc7d58dec20057e3c63e8618772a
marcelo.alves <marcelo.alves@pt.clara.net>
2025-04-11 15:46:00 +0100

    refactor: display user information in JSON format on home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,19 +6,24 @@ import { AuthService } from '../../services/auth.service';
   template: `
     <div class="py-8">
       <h1 class="text-2xl font-bold mb-6">Bem-vindo, {{ userName }}</h1>
+      <pre>
+          {{ allInfo | json }}
+        </pre
+      >
     </div>
   `,
 })
 export class HomeComponent implements OnInit {
   userName = '';
+  allInfo: any = {};
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     const user = this.authService.getUser();
+    this.allInfo = user;
     user.then((userData) => {
       if (userData) {
-        console.log('userData', userData);
         this.userName = userData.profile.name ?? 'Usuário';
       } else {
         this.userName = 'Usuário Desconhecido';
